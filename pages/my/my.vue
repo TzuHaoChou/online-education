1fa90b67545c54e3959342e2422eff5dc6c3f832
TzuHaoChou <2080399705@qq.com>
2022-12-01 15:34:30 +0800

    基础配置引入

---
+++ pages/my/my.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		<text class="iconfont icon-info">555</text>
+		<text class="iconfont icon-add"></text>
+		<view class="hhh"></view>
+		<view class="animate__animated animate__backOutRight">
+			555555555555555
+		</view>
+		<text class="animate__animated animate__backInLeft">8888888</text>
+		<!-- <view class="animate__animated animate__backOutDown">
+			555555555555555555555555555555555555
+		</view> -->
+		<!-- <view class="animate__animated animate__backInLeft">
+			hhhhhhhhhhhhhhhhhhhhhhh
+		</view> -->
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
